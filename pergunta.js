322a81a2e131bce3cd143b628ef72499a7f0306b
IRAZEstivel <161456781+IRAZEstivel@users.noreply.github.com>
2024-09-24 11:49:47 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Brawl stars',
+    'Quantas skins existem no brawl stars?',
+    '476 skins'
+)
+
+criaCartao(
+    'Brawl stars',
+    'No jogo Brawl stars, quando inicia o jogo pela primeira vez qual e o primeiro personagem que você começa no jogo?',
+    'Shelly'
+)
+
+criaCartao(
+    'Brawl stars',
+    'Quantos Brawlers tem atualmente no jogo?',
+    '83 Brawlers'
+)
+criaCartao(
+    'Brawl stars',
+    'O super do dynamike dá 6000 de dano, mais com hypercarga ele da 25% a mais de dano, quanto de dano ele da com a hypercarga ativada?',
+    '7500 de dano'
+)
+
+criaCartao(
+    'Brawl stars',
+    'Quantas gemas tem que pegar para vencer uma partida de pique gema?',
+    '10 Gemas'
+)
+
+criaCartao(
+    'Brawl stars',
+    'Quantos gols é necessario para vencer uma partida de fute brawl?',
+    '2 Gols'
+)
+criaCartao(
+    'Brawl stars',
+    'Qual e o maior rank de trofeu que consegue chegar em 1 Brawler?',
+    'Rank 35'
+)
+
+criaCartao(
+    'Brawl stars',
+    'Atualmente no jogo quem e o jogador com mais trofeu no global?',
+    'KCP/JuanCarlos'
+)
+
+criaCartao(
+    'Brawl stars',
+    'A piper e um personagem que quanto maior a distancia do inimigo ela da mais dano, Qual e o dano maximo dela?',
+    '3400 de dano'
+)
